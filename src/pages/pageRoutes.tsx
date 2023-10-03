de4c38029af70b30fc7a5d718911d42c51e998d5
rohitmeshramD <rohitmeshramd@gmail.com>
2023-10-03 23:20:18 +0530

    route not found issue resolved

--- src/pages/pageRoutes.tsx
+++ src/pages/pageRoutes.tsx
@@ -4,17 +4,15 @@ import { Route } from 'react-router-dom';
 import pagesData from './pagesData';
 
 export const defaultPageRoutes = pagesData
-  .map(({ path, element, title }) => {
-    
-    const routeArgs = { path, element };
-
-    return <Route {...routeArgs} key={title} />;
-  });
-
-export const authPageRoutes = pagesData
-  .filter((page) => !page.showDefault)
+  .filter((page) => page.showDefault)
   .map(({ path, element, title }) => {
     const routeArgs = { path, element };
 
     return <Route {...routeArgs} key={title} />;
   });
+
+export const authPageRoutes = pagesData.map(({ path, element, title }) => {
+  const routeArgs = { path, element };
+
+  return <Route {...routeArgs} key={title} />;
+});
