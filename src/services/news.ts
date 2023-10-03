703526ab237904adfee9b3f8f0a72a3f0c7cc747
rohitmeshramD <rohitmeshramd@gmail.com>
2023-10-04 00:21:54 +0530

    removeNulls from news list commented

--- src/services/news.ts
+++ src/services/news.ts
@@ -3,5 +3,6 @@ import { removeNulls } from 'helpers/object.helpers';
 export const newsServices = () => {
   return fetch(`${process.env.REACT_APP_SERVER_URL}/news`)
     .then((res) => res.json())
-    .then((res) => removeNulls(res));
+    // .then((res) => removeNulls(res));
+    .then((res) => res);
 };
